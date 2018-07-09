babaa3df8b0fa98c269ef8f907239fd71a23973d
omikron15 <omikron15@hotmail.co.uk>
2018-07-09 22:14:07 +0100

    Adds basic tests for getters of stock class to stock test file

--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -1,6 +1,9 @@
 import models.Stock;
+import models.StockType;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class StockTest {
 
@@ -10,11 +13,31 @@ public class StockTest {
     @Before
     public void setUp(){
         TEAstock = new Stock("TEA", StockType.Common, 0, 100);
-        GINstock = new Stock("GIN", StockType.Prefered, 0, 2, 100);
+//        GINstock = new Stock("GIN", StockType.Prefered, 0, 2, 100);
     }
 
     @Test
     public void getStockSymbol() {
         assertEquals("TEA", TEAstock.getSymbol());
+    }
+
+    @Test
+    public void getStockType() {
+        assertEquals(StockType.Common, TEAstock.getStockType());
+    }
+
+    @Test
+    public void getLastDividend() {
+        assertEquals(0, TEAstock.getLastDividend());
+    }
+
+    @Test
+    public void getFixedDividend() {
+        assertEquals(2, TEAstock.getFixedDividend());
+    }
+
+    @Test
+    public void getParValue() {
+        assertEquals(100, TEAstock.getParValue());
     }
 }
