dd9983fda09542ac0d1681f04791628f97a53422
omikron15 <omikron15@hotmail.co.uk>
2018-07-09 22:01:15 +0100

    Sets up stock test file with setup method

--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -1,2 +1,14 @@
+import models.Stock;
+import org.junit.Before;
+
 public class StockTest {
+
+    public Stock TEAstock;
+    public Stock GINstock;
+
+    @Before
+    public void setUp(){
+        TEAstock = new Stock("TEA", StockType.Common, 0, 100);
+        GINstock = new Stock("GIN", StockType.Prefered, 0, 2, 100);
+    }
 }
