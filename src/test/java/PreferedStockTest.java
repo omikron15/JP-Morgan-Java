f5ad66f74d261ecedf7041c6fa3e627b6f221bca
omikron15 <omikron15@hotmail.co.uk>
2018-07-10 13:29:06 +0100

    Adds prefered stock test file with basic tests

---
+++ src/test/java/PreferedStockTest.java
@@ -0,0 +1,43 @@
+import models.Stock;
+import models.StockType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PreferedStockTest {
+
+    public PreferedStock GINStock;
+
+    @Before
+    public void setUp(){
+        GINStock = new Stock("GIN", 8, 2, 100);
+    }
+
+    @Test
+    public void getStockSymbol() {
+        assertEquals("GIN", GINStock.getSymbol());
+    }
+
+    @Test
+    public void getStockType() {
+        assertEquals(StockType.Prefered, GINStock.getStockType());
+    }
+
+    @Test
+    public void getLastDividend() {
+        assertEquals(8, GINStock.getLastDividend());
+    }
+
+    @Test
+    public void getLastDividend() {
+        assertEquals(2, GINStock.getFixedDividend());
+    }
+
+    @Test
+    public void getParValue() {
+        assertEquals(100, GINStock.getParValue());
+    }
+
+
+}
